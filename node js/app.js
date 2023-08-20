2b8bd17d1c807f5b5e88a1316f530629ecba8b04
athulacaj <athuljacobacaj@gmail.com>
2023-08-20 16:48:34 +0530

    started adding integration testing

--- node js/app.js
+++ node js/app.js
@@ -1,41 +1,13 @@
 const app=require('express')();
 
-function middleware(req,res,next){
-    console.log('Middleware');
-    next();
-    console.log('Middleware next');
-}
-
-app.use(middleware);
-
-app.get('/',middleware,(req,res)=>{
-    res.send('Hello World');
-})
-
-app.get('/hello/',middleware,(req,res)=>{
-    res.send('Hello ....');
-})
-
-app.post('/',(req,res)=>{
-    res.send('post on /');
-})
 
 
-
-app.get('/about/:name/*/*',(req,res)=>{
-    console.log(req.params);
-    res.send('About Page 2 '+req.params.name+' '+req.params);
+app.get("/",(req,res)=>{
+    res.send({message:"Hello World"});
 });
 
-app.get('/parent/*', (req, res) => {
-    console.log(req.params);
-    res.send(`params: ${req.params}`);
-  });
-  
-
-
-app.get("*",(req,res)=>{
-    res.send('404');
+app.get("/json",(req,res)=>{
+    res.json({"msg": "hello"});
 });
 
 app.listen(4000,()=>{
